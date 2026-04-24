6dbccf2146db75391bcc33e5c95a41031a0de2e2
SanthiyaK <santhiyasantthosh@gmail.com>
2026-04-24 14:23:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App(){
   const [products,setProducts] = useState([]);
 
   useEffect(()=>{
-     fetch("http://localhost:5000/products")
+     fetch("http://13.203.221.45:5000/products")
      .then(res=>res.json())
      .then(data=>setProducts(data))
      .catch(err => console.error("Error fetching products:", err));
